dd49e6b6411b4d1ed6e2648fb102c2d288bb779e
Hugo Duksis <hugo.duksis@gmail.com>
2013-09-30 23:05:49 +0200

    Moving curl system calls to a seperate module

--- lib/who_will_review_my_pr/github.ex
+++ lib/who_will_review_my_pr/github.ex
@@ -1,25 +1,22 @@
 defmodule Github do
-
-  # TODO: get rid of curl
-
   def oauth_uri do
-    # TODO: implement redirect_uri & state
+    # TODO: implement state
     # http://developer.github.com/v3/oauth/#web-application-flow
-    "https://github.com/login/oauth/authorize?client_id=#{Config.client_id}&scope=repo"
+    "https://github.com/login/oauth/authorize?client_id=#{Config.client_id}&scope=repo&redirect_uri=#{URI.encode(Config.root_url)}"
   end
 
   def authenticate(code) do
     client_id = Config.client_id
     client_secret = Config.client_secret
-    command = "curl -XPOST 'https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}' 2>/dev/null"
-    response = System.cmd(command)
+    uri = "https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}&redirect_uri=#{URI.encode(Config.root_url)}"
+    response = HTTP.post(uri)
     Logger.debug(response)
     String.slice(response, 13, 40)
   end
 
   def user(access_token) do
-    command = "curl -XGET https://api.github.com/user?access_token=#{access_token} 2> /dev/null"
-    resp = System.cmd(command)
+    uri = "https://api.github.com/user?access_token=#{access_token}"
+    resp = HTTP.get(uri)
     {:ok, resp } = JSON.decode(resp)
     HashDict.get(resp, "login")
   end
@@ -38,8 +35,9 @@ defmodule Github do
   def ask_to_review(pull_request, reviewer, access_token) do
     [_, _, _, owner, repo, _, number] = String.split(pull_request, "/")
     comment = "Hey @#{reviewer} would you have time to review this? [/®](http://who-will-review-my-pr.herokuapp.com)"
-    command = "curl -H 'Authorization: token #{access_token}' -XPOST https://api.github.com/repos/#{owner}/#{repo}/issues/#{number}/comments -d '{ \"body\": \"#{comment}\" }' 2> /dev/null"
-    resp = System.cmd(command)
+    uri = "https://api.github.com/repos/#{owner}/#{repo}/issues/#{number}/comments"
+    request_body = "{ \"body\": \"#{comment}\" }"
+    resp = HTTP.post(access_token, uri, request_body)
     Logger.debug(resp)
     resp
   end
@@ -47,16 +45,17 @@ defmodule Github do
   defp collaborators(pull_request, access_token) do
     [_, _, _, owner, repo, _, _] = String.split(pull_request, "/")
 
-    command = "curl -H 'Authorization: token #{access_token}' -XGET https://api.github.com/repos/#{owner}/#{repo}/collaborators 2> /dev/null"
-    resp = System.cmd(command)
+    uri = "https://api.github.com/repos/#{owner}/#{repo}/collaborators"
+    resp = HTTP.get(access_token, uri)
     {:ok, resp } = JSON.decode(resp)
     Enum.map(resp, get_user_login(&1))
   end
 
   defp contributors(pull_request, access_token) do
     [_, _, _, owner, repo, _, _] = String.split(pull_request, "/")
-    command = "curl -H 'Authorization: token #{access_token}' -XGET https://api.github.com/repos/#{owner}/#{repo}/stats/contributors 2> /dev/null"
-    resp = System.cmd(command)
+
+    uri = "https://api.github.com/repos/#{owner}/#{repo}/stats/contributors"
+    resp = HTTP.get(access_token, uri)
     {:ok, resp } = JSON.decode(resp)
     Enum.map(resp, get_contribution_author(&1))
   end
---
+++ lib/who_will_review_my_pr/http.ex
@@ -0,0 +1,21 @@
+defmodule HTTP do
+  # TODO: get rid of curl use ibrowse instead
+  # https://github.com/cmullaparthi/ibrowse
+
+  def post(uri) do
+    System.cmd("curl -XPOST '#{uri}' 2>/dev/null")
+  end
+
+  def post(access_token, uri, request_body) do
+    System.cmd("curl -H 'Authorization: token #{access_token}' -XPOST #{uri} -d '#{request_body}' 2> /dev/null")
+  end
+
+  def get(uri) do
+    System.cmd("curl -XGET '#{uri}' 2>/dev/null")
+  end
+
+  def get(access_token, uri) do
+    System.cmd("curl -H 'Authorization: token #{access_token}' -XGET '#{uri}' 2>/dev/null")
+  end
+end
+
